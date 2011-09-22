a3de21bcd20323d6bd9947c79934e0834fcb4c4f
panmari <smmuzi@gmail.com>
2011-09-22 23:18:57 +0200

    Added date4j to Path, wrote some constructors
    Also first changes to use date4j

--- Calendar/src/calendar/CalendarMain.java
+++ Calendar/src/calendar/CalendarMain.java
@@ -1,5 +1,26 @@
 package calendar;
 
-public class CalendarMain {
+import java.util.LinkedList;
+import java.util.List;
 
+public class CalendarMain {
+	private List<User> userList;
+	
+	/**
+	 * Initializes a new userList
+	 */
+	public CalendarMain() {
+		userList = new LinkedList<User>();
+	}	
+	
+	/**
+	 * @param initialUsers  a List of already existing users
+	 */
+	public CalendarMain(LinkedList<User> initialUsers) {
+		this.userList = initialUsers;
+	}
+	
+	public static void main(String[] args) {
+		new CalendarMain();
+	}
 }
--- Calendar/src/calendar/Event.java
+++ Calendar/src/calendar/Event.java
@@ -1,15 +1,15 @@
 package calendar;
 
-import java.util.Date;
+import hirondelle.date4j.DateTime;
 
 public class Event {
 	
 	private String name;
-	private Date startTime;
-	private Date endTime;
+	private DateTime startTime;
+	private DateTime endTime;
 	private boolean isPrivate;
 
-	public Event(String name, Date startTime, Date endTime, boolean isPrivate) {
+	public Event(String name, DateTime startTime, DateTime endTime, boolean isPrivate) {
 		this.name = name;
 		this.startTime = startTime;
 		this.endTime = endTime;
