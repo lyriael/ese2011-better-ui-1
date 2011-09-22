23d50ec6eb639809bb06222fa9b9077d544ecf0b
panmari <smmuzi@gmail.com>
2011-09-22 18:02:13 +0200

    Made rough skeleton
    Still subject to change.

---
+++ Calendar/src/calendar/Event.java
@@ -0,0 +1,18 @@
+package calendar;
+
+import java.util.Date;
+
+public class Event {
+	
+	private String name;
+	private Date startTime;
+	private Date endTime;
+	private boolean isPrivate;
+
+	public Event(String name, Date startTime, Date endTime, boolean isPrivate) {
+		this.name = name;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.isPrivate = isPrivate;
+	}
+}
