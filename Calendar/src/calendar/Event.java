34636ad8c7ae76d9674f8f5b1307a910e1f43266
panmari <smmuzi@gmail.com>
2011-09-23 09:05:38 +0200

    Added some Constructors for Event.class, started Implementing comparison
    Trying to do it with a PriorityList. Would be pretty sweet, if everything worked out...
    DA ftw <.<

--- Calendar/src/calendar/Event.java
+++ Calendar/src/calendar/Event.java
@@ -1,22 +1,68 @@
 package calendar;
 
 import hirondelle.date4j.DateTime;
+import hirondelle.date4j.DateTime.DayOverflow;
 
-public class Event {
+public class Event implements Comparable {
 	
-	private String name;
+	private String eventName;
 	private DateTime startTime;
 	private DateTime endTime;
 	private boolean isPrivate;
 
-	public Event(String name, DateTime startTime, DateTime endTime, boolean isPrivate) {
-		this.name = name;
+	
+	/**
+	 * All conversion String <-> DateTime is done here for now
+	 * Subject to change! (Maybe implement special class for that)
+	 * 
+	 * @param EventName
+	 * @param startTime
+	 * @param endTime
+	 * @param isPrivate
+	 */
+	public Event(String eventName, DateTime startTime, DateTime endTime, boolean isPrivate) {
+		fillInData(eventName, startTime, endTime, isPrivate);
+	}
+	
+	public Event(String eventName, String date, String startTime) {
+		this(eventName, date, startTime, false);
+	}
+	
+	/**
+	 * Constructor without ending time. Makes an event with the lenght of one
+	 * hour by default. 
+	 * @param date
+	 * @param sTime
+	 * @param isPrivate
+	 */
+	public Event(String eventName, String date, String sTime, boolean isPrivate) {
+		//TODO: convert Strings so they're valid
+		//for now: organized panic.
+		DateTime startTime = new DateTime(date + " " + sTime);
+		DateTime endTime = startTime.plus(0, 0, 0, 1, 0, 0, DayOverflow.Spillover);
+		fillInData(eventName, startTime, endTime, isPrivate);
+	}
+
+	public boolean isPrivate(){
+		return isPrivate;
+	}
+	
+	private void fillInData(String eventName, DateTime startTime, DateTime endTime, boolean isPrivate) {
+		this.eventName = eventName;
 		this.startTime = startTime;
 		this.endTime = endTime;
 		this.isPrivate = isPrivate;
 	}
 	
-	public boolean isPrivate(){
-		return isPrivate;
+	public DateTime getStartTime() {
+		return startTime;
+	}
+
+	@Override
+	public int compareTo(Object arg0) {
+		if (arg0.getClass() != Event.class)
+			throw new IllegalArgumentException();
+		Event e = (Event) arg0;
+		return this.getStartTime().compareTo(e.getStartTime());
 	}
 }
--- Calendar/src/calendar/User.java
+++ Calendar/src/calendar/User.java
@@ -1,13 +1,39 @@
 package calendar;
 
+import hirondelle.date4j.DateTime;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+import java.util.TimeZone;
+
 public class User {
 	private String name;
+	private PriorityQueue<Event> eventList;
 	
 	public User (String name){
 		this.name = name;
+		eventList = new PriorityQueue<Event>();
 	}
 	
 	public String getName(){
 		return name;
 	}
+	
+	public Event addEvent(String eventName, String date, String time) {
+		Event newEvent = new Event(eventName, date, time);
+		return newEvent;
+	}
+	
+	private Event getNextEvent() {
+		for (Event e: eventList) {
+			DateTime startTime = e.getStartTime();
+			if (startTime.isInTheFuture(TimeZone.getDefault()))
+				return e;
+		}
+		throw new NoSuchElementException();
+	}
+	
+	
 }
