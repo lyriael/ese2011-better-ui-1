a7a8126899d20876d1230ca6473aaea07b793914
Jonas von Felten <jonas.vofi@sunrise.ch>
2011-09-27 08:50:12 +0200

    changed several methodes and wrote some tests

--- Calendar/src/calendar/Calendar.java
+++ Calendar/src/calendar/Calendar.java
@@ -27,19 +27,27 @@ public class Calendar implements Iterable<Event> {
 	 * @param user
 	 * @return
 	 */
-	public Iterator<Event> getVisibleEvents(User user) {
+	
+	
+	public Iterator<Event> getAllVisibleEvents(User user, String startDate) {
 		List<Event> publicEvents = new LinkedList<Event>();
-		if (user == owner)
-			return iterator();
+		List<Event> privateEvents= new LinkedList<Event>();
+		Event dummyEvent = new Event("dummy", startDate);
+		if (user == owner){
+			for (Event e: this){
+				if (e.compareTo(dummyEvent)>0)
+					privateEvents.add(e);
+			}
+			return privateEvents.iterator();
+		}
 		else {
 			for(Event e: this) {
-				if (!e.isPrivate())
+				if (!e.isPrivate() && e.compareTo(dummyEvent)>0)
 					publicEvents.add(e);
 			}
 			return publicEvents.iterator();
 		}
 	}
-
 	public String getCalendarName() {
 		return calendarName;
 	}
@@ -48,15 +56,36 @@ public class Calendar implements Iterable<Event> {
 		this.calendarName = calendarName;
 	}
 	
-	public List<Event> getListOfDate(String Date) {
+	public List<Event> getListOfDate(String Date, User user) {
+		
 		List<Event> ListOfSpecificDate = new LinkedList<Event>();
 		Event dummyEvent = new Event("dummy", Date);
-		for (Event e: this) {
-			e.hasSameStartingDateAs(dummyEvent);
-			ListOfSpecificDate.add(dummyEvent);
-			if (e.compareTo(dummyEvent) > 0) //coz its a priority List
-				break;
+		User tempUser= new User(null);
+		tempUser = user;
+		if (user==owner){
+			return getPrivateListofDate(ListOfSpecificDate, dummyEvent);
 		}
+		else{
+			return getPublicListofDate(ListOfSpecificDate, dummyEvent);
+		}
+				
+	}
+
+	private List<Event> getPublicListofDate(List<Event> ListOfSpecificDate,
+			Event dummyEvent) {
+		for (Event e: this) {
+			if (e.hasSameStartingDateAs(dummyEvent) && e.isPrivate()==false)
+				ListOfSpecificDate.add(e);
+		}
+		return ListOfSpecificDate;
+	}
+
+	private List<Event> getPrivateListofDate(List<Event> ListOfSpecificDate,
+			Event dummyEvent) {
+		for (Event e: this) {
+			if (e.hasSameStartingDateAs(dummyEvent))
+				ListOfSpecificDate.add(e);
+			}
 		return ListOfSpecificDate;
 	}
 	
@@ -73,5 +102,10 @@ public class Calendar implements Iterable<Event> {
 		}
 		throw new NoSuchElementException("No future event!");
 	}
+	
+	public User getOwner(){
+		return owner;
+		}
+	
 
 }
--- Calendar/src/calendarTests/testCalendar.java
+++ Calendar/src/calendarTests/testCalendar.java
@@ -2,6 +2,9 @@ package calendarTests;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,19 +20,44 @@ public class testCalendar {
 	public void setUp() throws Exception {
 		User testUser = new User("Vofi");
 		testCal = new Calendar("Vofi's Cal", testUser);
+		Event concert= testCal.addEvent("Crazy evening", "2011-02-18 18:00:00", "2011-02-18 23:00:00");
 		Event bdayParty = testCal.addEvent("Bday Party", "2011-02-19 18:00:00", "2011-02-19 20:00:00");
-		Event go2bed = testCal.addEvent("Zzz", "2011-02-19 22:00:00", "2011-02-20 08:00:00");
+		Event go2bed = testCal.addEvent("Zzz", "2011-02-19 22:00:00", "2011-02-20 23:00:00");
+		Event wakeUp = testCal.addEvent("GoodMorning", "2011-02-20 06:00:00", "2011-02-20 07:00:00");
 		
 	}
 
+	//@Test
+	//public void testCalendar() {
+	//	System.out.println(testCal.getNextEvent());
+	//}
+	
 	@Test
-	public void testCalendar() {
-		System.out.println(testCal.getNextEvent());
+	public void testShowListOfPrivateDate()
+	{
+		List<Event> testList =new LinkedList<Event>();
+		testList = testCal.getListOfDate("2011-02-19", testCal.getOwner());
+			System.out.println(testList.toString());
+			assert (testList.size()==2);
 	}
+	
+	@Test
+	public void testShowListOfPublicDate()
+	{
+		User testUser2 = new User("Stöfe");
+		List<Event> testList =new LinkedList<Event>();
+		testList = testCal.getListOfDate("2011-02-19", testUser2);
+		System.out.println(testList.size());
+		System.out.println(testList.toString());
+		assert (testList.size()==0);
+	}
+	
 
 	@Test
 	public void testIterator() {
-		fail("Not yet implemented");
+		List<Event> testList = new LinkedList<Event>();
+	System.out.println( testCal.getAllVisibleEvents(testCal.getOwner(), "2011-02-18"));
+		
 	}
 
 }
