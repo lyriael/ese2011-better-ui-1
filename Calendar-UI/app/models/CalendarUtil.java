e5387b82f524288b2d6627955e9dba77aa6f1680
lyriael <lyriael@gmx.net>
2011-10-11 08:34:54 +0200

    Stared implementing CalendarUtil
    new methods to get #of day of a month, and to roll moths.

--- Calendar-UI/app/models/CalendarUtil.java
+++ Calendar-UI/app/models/CalendarUtil.java
@@ -4,18 +4,49 @@ import java.util.Date;
 
 public class CalendarUtil {
 
-	private java.util.Calendar juc = java.util.Calendar.getInstance();
-	private Date today;
+	private static java.util.Calendar cal = java.util.Calendar.getInstance();
+	private static Date today;
+	private static CalendarUtil util;
 	
-	public CalendarUtil(){
-		// set today to the current date;
+	private CalendarUtil(){
+		today = new Date();
 	}
 	
-	public int getMonth(){
-		return juc.MONTH;
+	/**
+	 * Months are written in <code>int</code>'s from 0 to 11. <br>
+	 * Example: June would be <code> 5. </code>
+	 * Wrong input is not handled.
+	 * 
+	 * @param month
+	 */
+	public static CalendarUtil getInstanceToday(){
+		util = new CalendarUtil();
+		return util;	
 	}
 	
-	public int getFirstDayOfMonth(){
-		return juc.;
+	public static int getMonth(){
+		return cal.get(cal.MONTH);
+	}
+	/**
+	 * Sets the default time stamp one month ahead.
+	 */
+	public static void nextMonth(){
+		cal.add(cal.MONTH, 1);
+	}
+	
+	/**
+	 * Sets the default time stamp one month back.
+	 */
+	public static void previousMonth(){
+		cal.add(cal.MONTH, -1);
+	}
+	
+	public static int getNumberOfDaysOfMonth(){
+		return cal.getActualMaximum(cal.DATE);
+	}
+	
+	public static int getFirstSundayInMonth(){
+		//TODO
+		return 0;
 	}
 }	
---
+++ Calendar-UI/test/testCalendarUtil.java
@@ -0,0 +1,31 @@
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import hirondelle.date4j.DateTime;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.TimeZone;
+
+import models.Calendar;
+import models.CalendarUtil;
+import models.Event;
+import models.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.test.UnitTest;
+
+public class testCalendarUtil extends UnitTest{
+
+	@Test
+	public void shoudGiveInstance(){
+
+	}
+
+}
