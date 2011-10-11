13e468ef62a67d027c6f6ddeffdb55485238939a
lyriael <lyriael@gmx.net>
2011-10-11 07:39:39 +0200

    CalendarUtil for Date Parsing and stuff
    there isn't anything usefull yet, but anyway... I'm gona work on it now.

---
+++ Calendar-UI/app/models/CalendarUtil.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.Date;
+
+public class CalendarUtil {
+
+	private java.util.Calendar juc = java.util.Calendar.getInstance();
+	private Date today;
+	
+	public CalendarUtil(){
+		// set today to the current date;
+	}
+	
+	public int getMonth(){
+		return juc.MONTH;
+	}
+	
+	public int getFirstDayOfMonth(){
+		return juc.;
+	}
+}	
